2a6d79644ec48f8452e1abb66043fda7595d04e2
Gelver Vargas <gelkio@gmail.com>
2015-08-27 02:24:49 -0500

    Ajustes

--- js/application.js
+++ js/application.js
@@ -2,4 +2,5 @@
 window.MoviesApp = Ember.Application.create();
 
 MoviesApp.API_URL = 'https://api.themoviedb.org/3/';
+MoviesApp.IMAGES_URL = 'https://image.tmdb.org/t/p/w130';
 MoviesApp.API_KEY = '39aa1a462b9a8e9bc2349ed20bf87913';
--- js/router.js
+++ js/router.js
@@ -5,83 +5,189 @@ MoviesApp.Router.map(function() {
   });
 });
 
+MoviesApp.ResultsRoute = Ember.Route.extend({
+  model: function(params){
+    var url = MoviesApp.API_URL+'movie/now_playing?api_key='+MoviesApp.API_KEY;
+    return Ember.$.getJSON(url).then(function(data) {
+      return data.results;
+    });
+  }
+});
+
 MoviesApp.ResultsController = Ember.ArrayController.extend({
 	content: [],
+    actorsContent: [],
+    moviesContent: [],
 	text: '', //Search Text
 	
-	currentPage: 0,
-	totalPages: 0,
-	paginationClass: 'hidden',
+	actorCurrentPage: 1,
+	actorTotalPages: 0,
+	actorPaginationClass: 'hidden',
+    movieCurrentPage: 1,
+	movieTotalPages: 0,
+	moviePaginationClass: 'hidden',
 	
-	message: '',
-	messageClass: 'hidden',
+	actorMessage: '',
+	actorMessageClass: 'hidden',
+    movieMessage: '',
+	movieMessageClass: 'hidden',
+    
+    nowPlayingClass: 'show',
+    resultClass: 'hidden',
 	
 	actions: {
       
 	  searchActors: function(){
 		var self = this;
 		var text = self.get('text');
-		var url = MoviesApp.API_URL+'search/person?api_key='+MoviesApp.API_KEY+'&query='+text;
+		var urlActors = MoviesApp.API_URL+'search/person?api_key='+MoviesApp.API_KEY+'&query='+text;
+		var urlMovies = MoviesApp.API_URL+'search/movie?api_key='+MoviesApp.API_KEY+'&query='+text;
+          
+        self.set('nowPlayingClass', 'hidden');
+        self.set('resultClass', 'show');
+    
+		self.set('actorMessageClass', 'hidden');
+        self.set('movieMessageClass', 'hidden');
+        
+        Ember.$('#resultTabs a').click(function (e) {
+          e.preventDefault()
+          $(this).tab('show')
+        });
 		
-		self.set('messageClass', 'hidden');
-		
-		if(text != ""){
-		  Ember.$.getJSON(url).then(function(data) {
+		if(text !== ""){
+		  Ember.$.getJSON(urlActors).then(function(data) {
 			self.set('currentPage', data.page);
-		    self.set('totalPages', data.total_pages);
-			self.set('content', data.results);
+		    self.set('actorTotalPages', data.total_pages);
+			self.set('actorsContent', data.results);
 			
 			if(data.results.length > 0 ){
-			  self.set('paginationClass', 'show');
+			  self.set('actorPaginationClass', 'show');
 			}else{
-			  self.set('paginationClass', 'hidden');
-			  self.set('messageClass', 'show');
-			  self.set('message', 'There are no actors with the name '+text+'!');
+			  self.set('actorPaginationClass', 'hidden');
+			  self.set('actorMessageClass', 'show');
+			  self.set('actorMessage', 'There are no actors with the name '+text+'!');
+			}
+		  });
+          Ember.$.getJSON(urlMovies).then(function(data) {
+			self.set('currentPage', data.page);
+		    self.set('movieTotalPages', data.total_pages);
+			self.set('moviesContent', data.results);
+			
+			if(data.results.length > 0 ){
+			  self.set('moviePaginationClass', 'show');
+			}else{
+			  self.set('moviePaginationClass', 'hidden');
+			  self.set('movieMessageClass', 'show');
+			  self.set('movieMessage', 'There are no movies with the name '+text+'!');
 			}
 		  });
 	    }else{
-			self.set('content', []);
-            self.set('paginationClass', 'hidden');
-			self.set('messageClass', 'show');
-			self.set('message', 'Please put any text to make a search!');	
+			self.set('actorsContent', []);
+            self.set('moviesContent', []);
+            self.set('actorPaginationClass', 'hidden');
+			self.set('actorMessageClass', 'show');
+			self.set('actorMessage', 'Please put any text to make a search!');	
 		}
 	  },
 	  
-	  previousPage: function(){
+	  actorPreviousPage: function(){
 	    var self = this;
 		var text = self.get('text');
-		var currentPage = self.get('currentPage');
+		var currentPage = self.get('actorCurrentPage');
 		
 		if(currentPage > 1){
 		  currentPage --;
 		  var url = MoviesApp.API_URL+'search/person?api_key='+MoviesApp.API_KEY+'&query='+text+'&page='+currentPage;
 		  Ember.$.getJSON(url).then(function(data) {
-			self.set('currentPage', data.page);
-		    self.set('content', data.results);
+			self.set('actorCurrentPage', data.page);
+		    self.set('actorsContent', data.results);
 		  });
 		}
 	  },
 	  
-	  nextPage: function(){
+	  actorNextPage: function(){
 	    var self = this;
 		var text = self.get('text');
-		var currentPage = self.get('currentPage');
-		var totalPages = self.get('totalPages');
+		var currentPage = self.get('actorCurrentPage');
+		var actorTotalPages = self.get('actorTotalPages');
 		
-		if(currentPage < totalPages){
+		if(currentPage < actorTotalPages){
 		  currentPage ++;
 		  var url = MoviesApp.API_URL+'search/person?api_key='+MoviesApp.API_KEY+'&query='+text+'&page='+currentPage;
 		  Ember.$.getJSON(url).then(function(data) {
-		    self.set('currentPage', data.page);
-		    self.set('content', data.results);
+		    self.set('actorCurrentPage', data.page);
+		    self.set('actorsContent', data.results);
 		  });
 		}
-	  }
+	  },
+        
+      moviePreviousPage: function(){
+	    var self = this;
+		var text = self.get('text');
+		var currentPage = self.get('movieCurrentPage');
+		
+		if(currentPage > 1){
+		  currentPage --;
+		  var url = MoviesApp.API_URL+'search/movie?api_key='+MoviesApp.API_KEY+'&query='+text+'&page='+currentPage;
+		  Ember.$.getJSON(url).then(function(data) {
+			self.set('movieCurrentPage', data.page);
+		    self.set('moviesContent', data.results);
+		  });
+		}
+	  },
+	  
+	  movieNextPage: function(){
+	    var self = this;
+		var text = self.get('text');
+		var currentPage = self.get('movieCurrentPage');
+		var movieTotalPages = self.get('movieTotalPages');
+		
+		if(currentPage < movieTotalPages){
+		  currentPage ++;
+		  var url = MoviesApp.API_URL+'search/movie?api_key='+MoviesApp.API_KEY+'&query='+text+'&page='+currentPage;
+		  Ember.$.getJSON(url).then(function(data) {
+		    self.set('movieCurrentPage', data.page);
+		    self.set('moviesContent', data.results);
+		  });
+		}
+	  },
+        
+      actorsTab: function(){
+        Ember.$("#actorsResultPanel").addClass('active');
+        Ember.$("#moviesResultPanel").removeClass('active');
+      },
+        
+      moviesTab: function(){
+        Ember.$("#moviesResultPanel").addClass('active');
+        Ember.$("#actorsResultPanel").removeClass('active');
+      },
+        
+      showMovieDetail: function(id){
+        Ember.$("#myModal").modal('show');
+        var self = this;
+        var url = MoviesApp.API_URL+'movie/'+id+'?api_key='+MoviesApp.API_KEY;
+        Ember.$.getJSON(url).then(function(data) {
+          Ember.$('#movieTitle').html("");
+          Ember.$('#movieOverview').html("");
+		  self.set('movieTitle', data.original_title);
+		  self.set('movieOverview', data.overview);
+          self.set('moviePoster', MoviesApp.IMAGES_URL+data.poster_path);
+        });
+      }
 	}
 });
 
 MoviesApp.MoviesRoute = Ember.Route.extend({
   model: function(params){
+    var self = this;
+    
+    var actorDetailUrl = MoviesApp.API_URL+'person/'+params.actorId+'?api_key='+MoviesApp.API_KEY;
+    Ember.$.getJSON(actorDetailUrl).then(function(data) {
+      Ember.$('#actorName').html(data.name);
+      Ember.$('#actorBiography').html(data.biography);
+      Ember.$('#actorPhoto').attr('src', MoviesApp.IMAGES_URL+data.profile_path);
+    });
+    
     var url = MoviesApp.API_URL+'person/'+params.actorId+'/movie_credits?api_key='+MoviesApp.API_KEY;
     return Ember.$.getJSON(url).then(function(data) {
       return data.cast;
@@ -92,4 +198,16 @@ MoviesApp.MoviesRoute = Ember.Route.extend({
 MoviesApp.MoviesController = Ember.ArrayController.extend({
   sortProperties: ['release_date'],
   sortAscending: false,
+  actions: {
+    showMovieDetail: function(id){
+      Ember.$("#myModal").modal('show');
+      var self = this;
+      var url = MoviesApp.API_URL+'movie/'+id+'?api_key='+MoviesApp.API_KEY;
+      Ember.$.getJSON(url).then(function(data) {
+        Ember.$('#movieTitle').html(data.original_title);
+        Ember.$('#movieOverview').html(data.overview);
+        Ember.$('#moviePoster').attr('src', MoviesApp.IMAGES_URL+data.poster_path);
+      });
+    }
+  }
 });
